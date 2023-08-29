7ecc16e511c4108e8850614c0de8cb38e6a2410d
Kade <26305836+Kade-github@users.noreply.github.com>
2023-08-29 15:48:52 -0700

    Initial commit

---
+++ DedicatedServer/Logging.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DedicatedServer
+{
+    public class Logging
+    {
+        private FileStream stream;
+
+        /// <summary>
+        /// Private function to add data to the FileStream.
+        /// </summary>
+        /// <param name="value">Data</param>
+        private void AddText(string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value + "\n");
+            stream.Write(info, 0, info.Length);
+        }
+
+        /// <summary>
+        /// Get the date and time formatted without the stupid stuff.
+        /// </summary>
+        /// <returns>The formatted time and date.</returns>
+        private string GetDateTimeFormat()
+        {
+            return (DateTime.Now.ToLongDateString() + DateTime.Now.ToLongTimeString()).Replace(" ", "").Replace(",", "")
+                .Replace(":", "");
+        }
+
+        /// <summary>
+        /// Closes the FileStream, and flushes the contents to the file.
+        /// </summary>
+        public void Dispose()
+        {
+            stream.Flush();
+            stream.Close();
+        }
+        
+        /// <summary>
+        /// Creates a directory called "logs" if it doesn't exist,
+        /// then create a file with the current date and time and open it
+        /// as a filestream.
+        /// </summary>
+        public Logging()
+        {
+            if (!Directory.Exists("logs"))
+                Directory.CreateDirectory("logs");
+            string name = GetDateTimeFormat() + ".log";
+
+            stream = File.Create("logs/" + name);
+        }
+
+        /// <summary>
+        /// Writes an info message to the log.
+        /// </summary>
+        /// <param name="text">The info message.</param>
+        public void Info(string text)
+        {
+            string l = "[" + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + "] [Info] " + text;
+            
+            AddText(l);
+            Console.WriteLine(l);
+        }
+        
+        /// <summary>
+        /// Writes an error message to the log.
+        /// </summary>
+        /// <param name="text">The error message</param>
+        public void Error(string text)
+        {
+            string l = "[" + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + "] [Error] " + text;
+            
+            AddText(l);
+            Console.WriteLine(l);
+        }
+        
+        /// <summary>
+        /// Writes a warning message to the log.
+        /// </summary>
+        /// <param name="text">The warning message</param>
+        public void Warning(string text)
+        {
+            string l = "[" + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + "] [Warning] " + text;
+            
+            AddText(l);
+            Console.WriteLine(l);
+        }
+        
+        /// <summary>
+        /// Writes a fatal message to the log, calls Dispose() on end.
+        /// </summary>
+        /// <param name="text">The fatal message</param>
+        public void Fatal(string text)
+        {
+            string l = "[" + DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + "] [Fatal] " + text;
+            
+            AddText(l);
+            Console.WriteLine(l);
+            
+            Dispose();
+        }
+    }
+}
---
+++ DedicatedServer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using ENet;
+
+namespace DedicatedServer
+{
+    internal class Program
+    {
+        public static Logging log;
+        
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig) {
+            log.Dispose();
+            return true;
+        }
+        
+        
+        public static void Main(string[] args)
+        {
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+            log = new Logging();
+            
+            log.Info("MadnessTopDown Dedicated Server starting...");
+            
+            ENet.Library.Initialize();
+            using (Host server = new Host()) {
+                Address address = new Address();
+
+                address.Port = 5242;
+                server.Create(address, 3000);
+
+                Event netEvent;
+                log.Info("Started!");
+                while (!Console.KeyAvailable) {
+                    bool polled = false;
+                    
+                    while (!polled) {
+                        if (server.CheckEvents(out netEvent) <= 0) {
+                            if (server.Service(15, out netEvent) <= 0)
+                                break;
+
+                            polled = true;
+                        }
+
+                        switch (netEvent.Type) {
+                            case EventType.None:
+                                break;
+
+                            case EventType.Connect:
+                                log.Info("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Disconnect:
+                                log.Info("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Timeout:
+                                log.Info("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Receive:
+                                log.Info("Packet received from - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP + ", Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+                                netEvent.Packet.Dispose();
+                                break;
+                        }
+                    }
+                }
+
+                server.Flush();
+            }
+            log.Dispose();
+            ENet.Library.Deinitialize();
+        }
+        
+    }
+}
